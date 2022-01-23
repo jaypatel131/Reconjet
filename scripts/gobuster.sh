8c2ff5440b20139f7779ef1fcbbedc4f07b90b09
jaypatel131 <77262385+jaypatel131@users.noreply.github.com>
2022-01-23 10:03:44 +0530

    Delete scripts directory

--- scripts/gobuster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -d "$1" ]; then
-		mkdir $1
-fi
-if [ ! -d "$1/gobuster" ]; then
-		mkdir $1/gobuster
-fi
-gobuster dir -u $1 -w /usr/share/wordlists/dirb/common.txt --timeout 10s -o $PWD/$1/gobuster/gobuster.txt
--- scripts/whatweb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -d "$1" ]; then
-		mkdir $1
-fi
-if [ ! -d "$1/whatweb" ]; then
-		mkdir $1/whatweb
-fi
-whatweb -v $1 >> $PWD/$1/whatweb/$1+whatweb.com
